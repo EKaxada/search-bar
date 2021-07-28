143c6bcafb337c76b54aeeafe7798e24aae3aed9
ekaxada <enocksdha@gmail.com>
2021-07-28 16:27:57 +0300

    added sentence prediction (with dict words only)

--- src/App.js
+++ src/App.js
@@ -10,20 +10,24 @@ myTrie.insert("world");
 
 function App() {
   const [prefix, setPrefix] = useState("");
-  const [suggestion, setSuggestion] = useState('');
+  const [suggestion, setSuggestion] = useState("");
 
   const onChange = (e) => {
-    setPrefix(e.target.value);
-    var trie_prefix = e.target.value;
+    var value = e.target.value;
+    setPrefix(value);
+    var words = value.split(" ");
+    var trie_prefix = words[words.length - 1];
     var found_words = myTrie.find(trie_prefix).sort((a, b) => {
       return a.length - b.length;
     });
-    if (found_words.length>0) {
-      setSuggestion(found_words[0]);
-    } else if(found_words.length === 0){
-      setSuggestion('');
+    var first_word = found_words[0];
+    if (value !== "" && value[value.length - 1] !== " ") {
+      if (first_word!= null) {
+        var remainder = first_word.slice(trie_prefix.length);
+        setSuggestion(value + remainder);
+      }
     }
-  }
+  };
 
   return (
     <div className="App">
@@ -37,10 +41,11 @@ function App() {
         onChange={onChange}
       />
       <input
+        readOnly={true}
         type="text"
         name="search-bar2"
         id="search-bar2"
-        value={prefix.length>0?suggestion:''}
+        value={suggestion}
       />
     </div>
   );
